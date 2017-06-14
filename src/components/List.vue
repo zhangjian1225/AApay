3ba1e229785013a4a10adbbb3a238581a11e90ef
zhagjian <WB_zhangjian@51talk.com>
2017-06-14 14:25:48 +0800

    add

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="hello">
       <div class="container">
-           <ul>
-              <li></li>
-           </ul>
+          <div class="userImg"><div class="iconfont icon-xiaohai icon"></div></div>
+          <div class="content">
+              <div class="username commen">
+                  <p><span>账户</span><input type="number" ref="userName" placeholder="输入您的账号" /></p>
+              </div>
+              <div class="userpassword commen">
+                  <p><span>密码</span><input type="password" ref="show_hide" placeholder="输入您的密码" /><label class="iconfont icon-biyan" @click="showHide" ref="font"></label></p>
+              </div>
+              <div class="choose">
+                  <p><input type="checkbox" v-model='status'/><span>记住密码？</span><router-link to="/Register"><span class="now">立即注册</span></router-link></p>
+              </div>
+              <div class="login" @click="alert">登陆</div>
+              <h4 class="alert" ref="warn"></h4>
+          </div>
       </div>
   </div>
 </template>
@@ -13,13 +24,181 @@ export default {
   name: 'hello',
   data () {
     return {
+      type: true,
+      status: true
     }
   },
   methods: {
+    showHide () {
+      if (this.type) {
+        this.$refs.show_hide.setAttribute('type', 'text')
+        this.$refs.font.setAttribute('class', 'iconfont icon-yan')
+      } else {
+        this.$refs.show_hide.setAttribute('type', 'password')
+        this.$refs.font.setAttribute('class', 'iconfont icon-biyan')
+      }
+      this.type = !this.type
+    },
+    alert () {
+      const userName = this.$refs.userName.value
+      const passWord = this.$refs.show_hide.value
+      const warn = this.$refs.warn
+      if (userName === '' || passWord === '') {
+        warn.innerHTML = '账号密码不能为空!'
+        warn.classList.add('warn')
+        setTimeout(function () {
+          warn.classList.remove('warn')
+        }, 2500)
+      } else {
+        let option = {userName: userName, passWord: passWord}
+        this.$http.get('../../static/js/login.json', option).then(function (res) {
+          let data = JSON.parse(res.bodyText)
+          if (data.responseObject.code === '0') {
+            warn.innerHTML = '密码错误'
+            warn.classList.add('warn')
+            setTimeout(function () {
+              warn.classList.remove('warn')
+            }, 2500)
+          } else {
+            // 判断是否记住密码
+            let check = this.status
+            if (check) {
+              window.localStorage.setItem('userName', userName)
+              window.localStorage.setItem('passWord', passWord)
+            } else {
+              window.localStorage.removeItem('userName')
+              window.localStorage.removeItem('passWord')
+            }
+            this.$router.push('/Home')
+          }
+        })
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  
+  .hello{
+    width: 100%;
+    height: 100%;
+    background:url('../assets/bg.jpeg') no-repeat;
+    background-size:100% 100%; 
+  }
+  .container{
+    width: 100%;
+    position: absolute;
+    top: 15%;
+  }
+  .userImg{
+    width:4rem;
+    height: 4rem;
+    background:linear-gradient(to bottom right, purple , #fff );
+    border-radius: 100%;
+    margin: auto;
+    overflow: hidden; 
+    padding: .2rem;
+    .icon{
+      width: 100%;
+      height: 100%;
+      border-radius: 100%;
+      background: rgba(225,225,225,.5);
+      text-align: center;
+      line-height: 3.5rem;
+      font-size: 3rem;
+      color: #fff
+    }
+  }
+  .username{
+    margin-top: 2rem;
+  }
+  .userpassword label{
+    display: inline-block;
+    height: 2.5rem;
+    padding-right: .5rem;
+    line-height: 2.5rem;
+    font-size: 1rem;
+    color: #666;
+  }
+  .commen p{
+    width: 70%;
+    height: 2.5rem;
+    display: flex;
+    background: rgba(225,225,225,.5);
+    box-shadow: 1px 1px 10px #888888;
+    margin: auto;
+    span{
+      width: 20%;
+      display: inline-block;
+      height: 100%;
+      line-height: 2.5rem;
+      text-align: center;
+      font-size: .8rem;
+    }
+    input{
+      width: 80%;
+      height: 100%;
+      background: none;
+      border: none;
+      outline: none;
+      font-size: .8rem;
+    }
+  }
+  .choose{
+    width: 70%;
+    margin: auto;
+    margin-top: .5rem;
+    p{
+      input{
+        height: 1rem;
+        vertical-align:middle;
+      }
+      span{
+        font-size: .5rem;
+        display: inline-block;
+        height: .5rem;
+        padding-left: .2rem;
+        position: relative;
+        top: -.1rem;
+      }
+      .now{
+        float: right;
+        padding-top: .2rem;
+        color: white;
+      }
+    }
+  }
+  .login{
+    width: 70%;
+    height: 1.5rem;
+    background: linear-gradient(to bottom right, purple , #066fc0 );
+    margin: auto;
+    text-align: center;
+    line-height: 1.5rem;
+    font-size: .8rem;
+    color: #fff;
+    letter-spacing: .5rem;
+    margin-top: 1rem;
+  }
+  .alert{
+    width: 100%;
+    height: 0;
+    overflow: hidden;
+    text-align:center;
+    margin-top: 1rem;
+    color:#fff;
+    background: rgba(0,0,0,.5);
+  }
+.warn{
+  animation: wran 1.5s forwards;
+}
+ @keyframes wran{
+    from{
+      height: 0
+    }
+    to{
+      height: 1.5rem;
+      line-height: 1.5rem;
+    }
+ }
 </style>
--- src/components/Write.vue
+++ src/components/Write.vue
@@ -2,30 +2,33 @@
   <div class="hello">
     <div class="container">
       <div class="totle">
-        <div class="list">
-          <dl>
-            <dt><img src="../assets/girl.png"></dt>
-            <dd>张颖</dd>
-          </dl>
+        <div class="list" v-for="(val,i) in person" >
+            <dl v-if="val.sex=='girl'">
+              <dt><img src="../assets/girl.png"></dt>
+              <dd>{{val.name}}</dd>
+            </dl>
+            <dl v-if="val.sex=='boy'">
+              <dt><img src="../assets/boy.png"></dt>
+              <dd>{{val.name}}</dd>
+            </dl>
         </div>
-        <div class="list">
+        <div class="list" @click="addList">
           <dl>
-            <dt><img src="../assets/boy.png"></dt>
-            <dd>张健</dd>
+            <dt></dt>
+            <dd class="iconfont icon-tianjia"></dd>
           </dl>
-        </div>
-        <div class="list">
-          <dl>
-            <dt><img src="../assets/girl.png"></dt>
-            <dd>张颖</dd>
-          </dl>
-        </div>
-        <div class="list">
-          <p class="iconfont icon-tianjia"></p>
         </div>
       </div>
       <div class="write"></div>
-      <div class="add"></div>
+      <div class="add" ref="addList">
+        <div class="auto">
+            <span @click="close">X</span>
+            <p><input type="radio" value="boy" v-model="sex"/> 男 <input type="radio" value="girl" v-model="sex"/> 女</p>
+            <p>新增人姓名：<input type="text" ref="name"/></p>
+            <p><button class="addBtn" @click="add">添加</button></p>
+        </div>
+      </div>
+      <div class="say" ref="warn"></div>
     </div>
   </div>
 </template>
@@ -35,9 +38,46 @@ export default {
   name: 'hello',
   data () {
     return {
+      sex: 'boy',
+      person: [
+        {name: '张颖', sex: 'girl'},
+        {name: '张健', sex: 'boy'},
+        {name: '张蕊', sex: 'girl'}
+      ]
     }
   },
   methods: {
+    addList () {
+      this.$refs.addList.classList.add('unflod')
+      this.$refs.addList.classList.remove('up')
+    },
+    close () {
+      this.$refs.addList.classList.add('up')
+      this.$refs.addList.classList.remove('unflod')
+    },
+    add () {
+      let value = this.$refs.name.value
+      let warn = this.$refs.warn
+      if (value === '') {
+        warn.innerHTML = '填写不能为空！'
+        warn.classList.add('warn')
+        setTimeout(function () {
+          warn.classList.remove('warn')
+        }, 2500)
+      } else {
+        let option = {name: value, sex: this.sex}
+        console.log(option)
+        this.ajaxs('../../static/js/login.json', option, this.addArr)
+      }
+    },
+    ajaxs (url, option, fn) {
+      this.$http.get(url, option).then(function (res) {
+        fn()
+      })
+    },
+    addArr () {
+      alert()
+    }
   }
 }
 </script>
@@ -59,14 +99,6 @@ export default {
         margin-left: .2rem;
         margin-top: .2rem;
         background: linear-gradient(to bottom right, purple , pink );
-        p{
-          width: 100%;
-          height: 100%;
-          text-align: center;
-          line-height: 6.25rem;
-          font-size: 3rem;
-          color: #fff;
-        }
         dl{
           width: 100%;
           height:100%;
@@ -90,14 +122,109 @@ export default {
         }
       }
     }
-    .write{
+    .list:last-child{
+        dl{
+          width: 100%;
+          height: 100%;
+          padding: 0;
+          dt{
+            height: 0;
+          }
+          dd{
+            width: 100%;
+            margin: 0;
+            height: 6.15rem;
+            text-align: center;
+            font-size:3rem;
+            color: #fff;
+            line-height: 6.15rem;
+          }
+        }
+      };
+    .add{
       width: 98.8%;
-      height: 8rem;
+      height: 0rem;
       background: blue;
       float: left;
       margin-left: .1rem;
       background: linear-gradient(to bottom right, #fff , pink );
+      position: relative;
+      overflow: hidden;
+      .auto{
+        position: relative;
+        span{
+          position: absolute;
+          right: 0;
+          top: -.5rem;
+          height: 1rem;
+          width: 1rem;
+          background: #fff;
+          text-align: center;
+          line-height: 1rem;
+        }
+      }
+      p{
+        color: purple;
+        font-family: "sans-serif";
+        margin-top: .8rem;
+        margin-left: 1rem;
+        input[type^="radio"]{
+          width:1rem;
+          height: 1rem;
+          vertical-align:middle;
+          color: white;
+          margin-right: 1rem;
+        };
+        input[type^="text"]{
+          width:5rem;
+          height: 1.5rem;
+          vertical-align:middle;
+          color:purple;
+          border-color:rgba(141,39,142,.75); 
+          box-shadow:0 0 18px rgba(111,1,32,3); 
+        };
+        button{
+          width: 3rem;
+          height: 1.5rem;
+          color: purple;
+          background: linear-gradient(to bottom right, #fff , #666 );
+          margin: .5rem auto;
+          font-size: .8rem;
+          border: 1px solid #fff;
+
+        }
+      }
     }
+    .add.unflod{
+      animation: unflod .5s forwards
+    }
+    .add.up{
+      animation: up .5s forwards
+    }
+    @keyframes up{
+      0%{
+        height: 8rem;
+      }
+      100%{
+        height: 0rem;
+      }
+    } 
+    @keyframes unflod{
+      0%{
+        height: 0;
+      }
+      100%{
+        height: 8rem;
+      }
+    } 
+    /* .auto{
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom:0;
+      right:0;
+      margin: auto;
+    } */
     /* .add{
       width: 98.8%;
       height: 20px;
@@ -105,5 +232,26 @@ export default {
       float: left;
       margin-left: .1rem;
     } */
+    .say{
+      width: 100%;
+      height: 0;
+      overflow: hidden;
+      text-align:center;
+      margin-top: 1rem;
+      color:#fff;
+      background: rgba(0,0,0,.5)
+    }
+    .say.warn{
+      animation: wran 1.5s forwards;
+    }
+     @keyframes wran{
+        from{
+          height: 0
+        }
+        to{
+          height: 1.5rem;
+          line-height: 1.5rem;
+        }
+     }
   }
 </style>
